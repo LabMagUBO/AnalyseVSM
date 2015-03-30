9e127fa1bda88cfed248575c6542efb9f7451458
Jérôme Richy <jerome.richy@univ-brest.fr>
2015-03-31 00:45:50 +0200

    Ajout des classes / Rotation terminé

--- __init__.py
+++ __init__.py
@@ -1,21 +1,24 @@
-####################
-# Modules à  importer
-####################
-#Modules de log / affichage
-#import logging
-
-# Gestion des fichiers/dossiers
-import os
-import sys
-import shutil
-import fnmatch      # for pattern matching
-import re           # for regular expression
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    Fichier d'initialisation du module.
+    À importer en premier.
+"""
+# Modules généraux
+#import os
+#import sys
+#import shutil
+#import fnmatch      # for pattern matching
+#import re           # for regular expression
 
 #from os import path, makedirs, listdir
 # Paquets science
-import numpy as np
-from matplotlib import pylab as pl
-from scipy import optimize
+#import numpy as np
+#from matplotlib import pylab as pl
+#from scipy import optimize
 
-from AnalyseVSM.variables import *
-from AnalyseVSM.analyse import *
+
+# Sous-modules «AnalyseVSM»
+from AnalyseVSM.constantes import *
+from AnalyseVSM.Mesures import *
+#from AnalyseVSM.analyse import *
--- testing/processVSM.py
+++ testing/processVSM.py
@@ -10,21 +10,64 @@
         -> python processVSM.py data/
 """
 
-####################
-# Importation «AnalyseVSM»
-####################
 ## Si le module n'est pas installé (path python), on lui directement le chemin
 module_path = '/Users/zorg/These/Documents/Programmes/Python_Modules/'
 import sys
 sys.path.append(module_path)
 
-# on peut alors l'importer
+# On peut alors importer le module princippal
 from AnalyseVSM import *
+
+
+## Création de l'environnement
+scpy = Mesures()
+
+## Modification des paramètres par défaut
+scpy.dos_cycles = ''
+
+####################
+# Varibles / Constantes
+####################
+# Ces variables permette de définir le comportement des méthodes.
+## Paramètres courbes
+#zoom
+scpy.H_min = -20
+scpy.H_max = 20
+
+scpy.n_Hsat = 20     # nombre de points utilisé pour déterminer la pente
+scpy.H_biais = 0.3  # biais de la sonde de hall(en Oe)
+
+###### Variable
+# Dossiers
+#dos_cycles -> en argument du script
+scpy.dos_plot = "pdf"
+scpy.dos_export = "xdat"
+
+####################
+# Analy
+####################
+# Centrage de Ml et Mt.
+scpy.centrer = True
+
+
+####################
+# Varibles / Constantes
+####################
+# Permet de tracer la rotation. L'analyse prend alors en compte les angles selon le prefix et le suffix, et n'analyse pas les autres fichiers.
+# ex : fichier_001deg_n1.dat
+#   -> prefix = "*chier_"
+#   -> suffix "_n*"
+scpy.rotation = True
+scpy.prefix = "rot_"
+scpy.suffix = "deg_"
+scpy.file_rotation = 'rotation'
+
+# On initialise
+scpy.init()
 
 
 
 ####################
 # Lancement du programme
 ####################
-
-run_analyse()
+scpy.run_analyse()
